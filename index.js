c8bae39c136cea0dfa0fc3e59e22c6bc76aaeb0f
nanipinninti <155393108+nanipinninti@users.noreply.github.com>
2024-09-02 15:07:14 +0530

    cors update

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-
+app.use(cors(
+    {
+        origin : "*",
+        methods : ["GET","POST", "PUT", "DELETE"],
+        allowedHeaders : ['Content-Type','Authorization'],
+        optionsSuccessStatStatus : 204,
+    }
+));
 const mongoURI = "mongodb+srv://users:nani123@cluster0.jvt0su3.mongodb.net/Students?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
